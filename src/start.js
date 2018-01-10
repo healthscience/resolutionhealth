c215b7f4541d4b438d41677b7f9fe820f4dcfe0f
aboynejames <james@aboynejames.co.uk>
2018-01-10 15:25:23 +0000

    start data collect chart on heart click

--- src/start.js
+++ src/start.js
@@ -16,6 +16,47 @@ $(document).ready(function(){
 	// check public private key is present direct or via guardian
 	// if present, login  (prompt for password if steup)
 	// display live heart data UI
+
+function isElectron() {
+  //return process && process.versions && (process.versions.electron !== undefined);
+	if (typeof require !== 'function') return false;
+	  if (typeof window !== 'object') return false;
+	  try {
+	    const electron = require('electron');
+	    if (typeof electron !== 'object') return false;
+	  } catch(e) {
+	    return false;
+	  }
+	  return true;
+}
+
+var liveele = isElectron();
+if(liveele == true)
+{
+	// for electron
+	var fs = require('fs');
+	readFile('./src/key/temptoken.txt');
+	function readFile(filepath) {
+				 fs.readFile(filepath, 'utf-8', function (err, data) {
+						 if(err){
+								 alert("An error ocurred reading the file :" + err.message);
+								 return;
+						 }
+console.log(data);
+						 tempToken = data;
+						 $("#signed-in").show();
+						 $("#connectivity").show();
+						 $("#life-health-menu").show();
+						 $("#authorisation-in").data("live-account");
+						 $("#authorisation-in").data("live-account", "on");
+						 $("#ptop-view").hide();
+						 $("#science-view").hide();
+						 $("#wearable-chart").hide();
+						 $("#science-view").hide();
+				 });
+		 }
+}
+else {
 	var tempToken = '';
 	fetch('key/temptoken.txt')
   .then(response => response.text())
@@ -30,8 +71,8 @@ $(document).ready(function(){
 		$("#science-view").hide();
 		$("#wearable-chart").hide();
 		$("#science-view").hide();
-	}
-	);
+	});
+}
 
 	$("a").click(function(e) {
 		e.preventDefault(e);
@@ -162,11 +203,151 @@ console.log('get required heart data');
 console.log(response);
 			let heartData = response.data;
 			let message = "Heart Data Loaded";
+
+			/* charting */
+
+				var chartColors = {
+					red: 'rgb(255, 99, 132)',
+					orange: 'rgb(255, 159, 64)',
+					yellow: 'rgb(255, 205, 86)',
+					green: 'rgb(75, 192, 192)',
+					blue: 'rgb(54, 162, 235)',
+					purple: 'rgb(153, 102, 255)',
+					grey: 'rgb(201, 203, 207)'
+				};
+
+				function newDate(ms) {
+					return moment().add(ms, 'ms');
+				}
+
+				function randomScalingFactor() {
+					return (Math.random() > 0.5 ? 1.0 : -1.0) * Math.round(Math.random() * 100);
+				}
+
+				function onRefresh() {
+					config.data.labels.push(moment());
+					config.data.datasets.forEach(function(dataset) {
+						dataset.data.push(randomScalingFactor());
+					});
+				}
+
+				var color = Chart.helpers.color;
+				var config = {
+					type: 'bar',
+					data: {
+						labels: [],
+						datasets: [{
+							label: 'Dataset (line)',
+							type: 'line',
+							backgroundColor: color(chartColors.red).alpha(0.5).rgbString(),
+											borderColor: chartColors.red,
+							fill: false,
+							cubicInterpolationMode: 'monotone',
+							data: []
+						}, {
+							label: 'Dataset (bars)',
+							backgroundColor: color(chartColors.blue).alpha(0.5).rgbString(),
+							borderColor: chartColors.blue,
+											borderWidth: 1,
+							data: []
+						}]
+					},
+					options: {
+						responsive: true,
+						title: {
+							display: true,
+							text: 'Mixed chart (horizontal scroll) sample'
+						},
+						scales: {
+							xAxes: [{
+								type: 'realtime',
+								display: true,
+								time: {
+									unitStepSize: 1
+								}
+							}],
+							yAxes: [{
+								type: 'linear',
+								display: true,
+								scaleLabel: {
+									display: true,
+									labelString: 'value'
+								}
+							}]
+						},
+						tooltips: {
+							intersect: false
+						},
+						hover: {
+							mode: 'nearest',
+							intersect: false
+						},
+						plugins: {
+							streaming: {
+								duration: 20000,
+								refresh: 1000,
+								delay: 2000,
+								onRefresh: onRefresh
+							}
+						}
+					}
+				};
+
+				//window.onload = function() {
+		console.log('canvas charting starting');
+					var ctx = document.getElementById('canvas').getContext('2d');
+					window.myBar = new Chart(ctx, config);
+				//};
+
+				document.getElementById('randomizeData').addEventListener('click', function() {
+					config.data.datasets.forEach(function(dataset) {
+						for (var i = 0; i < dataset.data.length; ++i) {
+							dataset.data[i] = randomScalingFactor();
+						}
+					});
+
+					window.myBar.update();
+				});
+
+				var colorNames = Object.keys(chartColors);
+				document.getElementById('addDataset').addEventListener('click', function() {
+					var colorName = colorNames[config.data.datasets.length % colorNames.length];
+					var newColor = chartColors[colorName];
+					var newDataset = {
+						label: 'Dataset ' + config.data.datasets.length,
+						type: 'line',
+						backgroundColor: color(newColor).alpha(0.5).rgbString(),
+						borderColor: newColor,
+						fill: false,
+						cubicInterpolationMode: 'monotone',
+						data: new Array(config.data.labels.length)
+					};
+
+					config.data.datasets.push(newDataset);
+					window.myBar.update();
+				});
+
+				document.getElementById('removeDataset').addEventListener('click', function() {
+					config.data.datasets.pop();
+					window.myBar.update();
+				});
+
+				document.getElementById('addData').addEventListener('click', function() {
+					onRefresh();
+
+					window.myBar.update();
+				});
+
+
+
+
+
+
 		})
 		.catch(function (error) {
     console.log(error);
-  });
-}
+  	});
+	}
 
 
 /*  Socket listeners */
@@ -189,139 +370,6 @@ console.log(response);
 		$("#maidsafe-api-status").text('live');
 		$("#maidsafe-api-status").css("background-color", "green");
 
-	});
-
-/* charting */
-
-	var chartColors = {
-		red: 'rgb(255, 99, 132)',
-		orange: 'rgb(255, 159, 64)',
-		yellow: 'rgb(255, 205, 86)',
-		green: 'rgb(75, 192, 192)',
-		blue: 'rgb(54, 162, 235)',
-		purple: 'rgb(153, 102, 255)',
-		grey: 'rgb(201, 203, 207)'
-	};
-
-	function newDate(ms) {
-		return moment().add(ms, 'ms');
-	}
-
-	function randomScalingFactor() {
-		return (Math.random() > 0.5 ? 1.0 : -1.0) * Math.round(Math.random() * 100);
-	}
-
-	function onRefresh() {
-		config.data.labels.push(moment());
-		config.data.datasets.forEach(function(dataset) {
-			dataset.data.push(randomScalingFactor());
-		});
-	}
-
-	var color = Chart.helpers.color;
-	var config = {
-		type: 'bar',
-		data: {
-			labels: [],
-			datasets: [{
-				label: 'Dataset (line)',
-				type: 'line',
-				backgroundColor: color(chartColors.red).alpha(0.5).rgbString(),
-								borderColor: chartColors.red,
-				fill: false,
-				cubicInterpolationMode: 'monotone',
-				data: []
-			}, {
-				label: 'Dataset (bars)',
-				backgroundColor: color(chartColors.blue).alpha(0.5).rgbString(),
-				borderColor: chartColors.blue,
-								borderWidth: 1,
-				data: []
-			}]
-		},
-		options: {
-			responsive: true,
-			title: {
-				display: true,
-				text: 'Mixed chart (horizontal scroll) sample'
-			},
-			scales: {
-				xAxes: [{
-					type: 'realtime',
-					display: true,
-					time: {
-						unitStepSize: 1
-					}
-				}],
-				yAxes: [{
-					type: 'linear',
-					display: true,
-					scaleLabel: {
-						display: true,
-						labelString: 'value'
-					}
-				}]
-			},
-			tooltips: {
-				intersect: false
-			},
-			hover: {
-				mode: 'nearest',
-				intersect: false
-			},
-			plugins: {
-				streaming: {
-					duration: 20000,
-					refresh: 1000,
-					delay: 2000,
-					onRefresh: onRefresh
-				}
-			}
-		}
-	};
-
-	window.onload = function() {
-		var ctx = document.getElementById('canvas').getContext('2d');
-		window.myBar = new Chart(ctx, config);
-	};
-
-	document.getElementById('randomizeData').addEventListener('click', function() {
-		config.data.datasets.forEach(function(dataset) {
-			for (var i = 0; i < dataset.data.length; ++i) {
-				dataset.data[i] = randomScalingFactor();
-			}
-		});
-
-		window.myBar.update();
-	});
-
-	var colorNames = Object.keys(chartColors);
-	document.getElementById('addDataset').addEventListener('click', function() {
-		var colorName = colorNames[config.data.datasets.length % colorNames.length];
-		var newColor = chartColors[colorName];
-		var newDataset = {
-			label: 'Dataset ' + config.data.datasets.length,
-			type: 'line',
-			backgroundColor: color(newColor).alpha(0.5).rgbString(),
-			borderColor: newColor,
-			fill: false,
-			cubicInterpolationMode: 'monotone',
-			data: new Array(config.data.labels.length)
-		};
-
-		config.data.datasets.push(newDataset);
-		window.myBar.update();
-	});
-
-	document.getElementById('removeDataset').addEventListener('click', function() {
-		config.data.datasets.pop();
-		window.myBar.update();
-	});
-
-	document.getElementById('addData').addEventListener('click', function() {
-		onRefresh();
-
-		window.myBar.update();
 	});
 
 });
